47f78efb11108a8c20b7a1ac3c46985c4de16352
ayushjain12901 <61902785+ayushjain12901@users.noreply.github.com>
2021-08-30 07:28:26 +0530

    Create query; for creating table of each country. Ignored the header and trailer row after reading the file
    Created a dynamic query, so that it can be changed based on schema! Could have set only country name as dynamic , but for making changes easily in schema, I have created whole query as dynamic (including constraints)

--- 18BCE082_Python_sql.py
+++ 18BCE082_Python_sql.py
@@ -19,6 +19,33 @@ schema = {
 
 
 
+# Query for table creation
+def create_table(split_data):
+	country = split_data[8] # Represents Country name
+
+	query="Create table "+ country + "("
+
+	for col in schema:
+		query = query + col + " " + schema[col][1]
+
+		#If its a date attribute, then no need to specify length
+		if schema[col][1] != "DATE":
+			query = query + "("+ str(schema[col][0]) +") "
+
+		if(schema[col][2] == 'Y'):
+			query = query + " NOT NULL"
+
+		if (schema[col][3] == 'Y'):
+			query = query + " PRIMARY KEY "
+
+		if(col != "Active_Customer"): # If its last column, then dont add ','
+			query = query + ", "
+
+	return query + ")"
+	
+
+
+
 #Open the file in read mode
 f = open("Incubyte_data.txt","r")
 
@@ -26,6 +53,8 @@ f = open("Incubyte_data.txt","r")
 mydb = mysql.connector.connect(host="localhost", user="ayush", passwd="Ayushjain1290#", database="ayush")
 mycursor = mydb.cursor() #Gives a pointer to database
 
+#Set for storing the tablenames that are already present
+s=set()
 
 #Read file line by line
 for line in f:
@@ -42,6 +71,20 @@ for line in f:
 	#Convert list into tuple for insert query
 	splitted_data=tuple(splitted_data)
 
+
+	#Ignore the line, if its header or trailer
+	if(splitted_data[0] == 'H' or splitted_data[0] == 'T' ):
+		continue
+	elif (splitted_data[0] == 'D'):
+		if(splitted_data[8] not in s):  # Check if table already exists
+			s.add(splitted_data[8])
+			mycursor.execute(create_table(splitted_data)) # Execute create table
+
+
+# Commit data in database and deallocate cursor
+mydb.commit()
+mycursor.close()
+mydb.close()
 
 
 #Close the file		
